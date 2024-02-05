f29d74e8947cc1aaf7d3c5f98df6a28044238e08
pascualmg <pascual.munoz@pccomponentes.com>
2024-02-05 22:59:54 +0100

    doom

---
+++ doom/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(rainbow-delimiters ac-php emojify base16-theme spacemacs-theme @))
+ '(warning-suppress-types '((emacs) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'exec-path "/nix/store/5jpylhcyw6rwnd09hlsrf2m6alm82vvz-php-with-extensions-8.2.11/bin/php")
