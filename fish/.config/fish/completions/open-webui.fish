f84e71dac0c6de1b19e00f8b6c57f39f63fddb79
Pascual Muñoz Galian <pmunozg@ces.vocento.com>
2025-06-10 13:35:44 +0200

    fish

---
+++ fish/.config/fish/completions/open-webui.fish
@@ -0,0 +1,1 @@
+complete --command open-webui --no-files --arguments "(env _OPEN_WEBUI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) open-webui)" --condition "env _OPEN_WEBUI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) open-webui"
